4db23cc9530f73f77388851656c912cc1b39590e
MegumuIizunamaru <chopin.won@gmail.com>
2024-11-26 21:29:36 +0700

    Changed stuff, y'know?

--- genshin.py
+++ genshin.py
@@ -6,15 +6,12 @@ genshin = pd.read_csv("genshin.csv",encoding='ISO-8859-1')
 st.title("Genshin Impact Data Analysis")
 
 DF = genshin.groupby('vision').agg({'hp_1_20':'mean'})
-DF = DF.reset_index()
 
 st.title("Genshin Dashboard")
 
 filtered_data = DF['vision']
 
 st.subheader("Genshin Dashboard")
-
-st.write(filtered_data)
 
 st.bar_chart(DF["hp_1_20"])
 # D:\DataScience\genshin.py
