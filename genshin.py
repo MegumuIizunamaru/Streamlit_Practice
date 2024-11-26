11f37b3c2ebba3046ec172600d946717f26a1623
MegumuIizunamaru <chopin.won@gmail.com>
2024-11-26 21:31:59 +0700

    Update genshin.py

--- genshin.py
+++ genshin.py
@@ -9,8 +9,6 @@ DF = genshin.groupby('vision').agg({'hp_1_20':'mean'})
 
 st.title("Genshin Dashboard")
 
-filtered_data = DF['vision']
-
 st.subheader("Genshin Dashboard")
 
 st.bar_chart(DF["hp_1_20"])
