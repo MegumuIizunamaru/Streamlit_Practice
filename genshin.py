da9a778a3356d5f780b4ad681a34d37f078659c9
MegumuIizunamaru <chopin.won@gmail.com>
2024-11-26 17:15:06 +0700

    Add files via upload

---
+++ genshin.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+genshin = pd.read_csv("DataSciPractice3/genshin.csv",encoding='ISO-8859-1')
+
+st.title("Genshin Impact Data Analysis")
+
+DF = genshin.groupby('vision').agg({'hp_1_20':'mean'})
+DF = DF.reset_index()
+
+st.title("Genshin Dashboard")
+
+filtered_data = DF['vision']
+
+st.subheader("Genshin Dashboard")
+
+st.write(filtered_data)
+
+st.bar_chart(DF)
+# D:\DataScience\genshin.py
